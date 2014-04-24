56939661cefbcdb8407b8318489530986bf83d8b
andrewwongrolle <awongrolle@college.harvard.edu>
2014-04-24 02:08:30 -0400

    wrote csv parse part

--- analyze.py
+++ analyze.py
@@ -1,11 +1,27 @@
 #analysis of song file module
+#library for working with csvs
+import csv
 
-#from the filepath 'file' and return the filepath to a new file containing the melody information
+#from the filepath 'file' and return an array containing the melody information
 def get_melody(file):
-	raise Exception('Not implemented')
+	melody_array = []
+	with open('file','rb') as raw_melody
+	reader = csv.reader(raw_melody)
+	for row in reader:
+		timestamp,frequency = row
+		melody_array.append(timestamp,frequency)
+	return melody_array
 
 #from the melody file, generate an array of midi values collected at regular intervals
 def process_melody(melody):
+	midi_array = []
+	diff_array = []	
+	for (timestamp,frequency) in melody:
+		midi_array.append(timestamp,freq_to_midi(frequency))
+	for x in xrange(0,len(midi_array)):
+		time1,freq1 = midi_array.x 
+		time2,freq2 = midi_array.(x+1)
+		diff_array.append((time1,freq2-freq1))
 	raise Exception('Not implemented')
 
 def freq_to_midi(freq):
