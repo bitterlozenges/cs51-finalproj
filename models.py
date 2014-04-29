3fdac28017961eaf65f1d109f338fe639024fe6e
Tiffany Wu <haotianwu@college.harvard.edu>
2014-04-28 21:29:02 -0400

    done with populate.py without testing

--- models.py
+++ models.py
@@ -6,9 +6,16 @@ class Song:
 	Represents a song, that, when initialized, generates the spectrograph, the array of 
 	intense parts, has a method that can be called to insert the song into the database
 	"""
-	def __init__(self,file_path):
+	def __init__(self,file_path,melody=None,starts=None):
 		self.file_path = file_path
-		self.gen_melody()
+		if melody = None:
+			self.gen_melody()
+		else:
+			self.melody = melody
+			if starts = None:
+				self.gen_starts()
+			else:
+				self.starts = starts
 		return
 
 	# stores the tuple list of (time,midi_val) from the file path in melody
@@ -16,7 +23,21 @@ class Song:
 		self.melody = get_melody(file_path)
 		return
 
+	def gen_starts(self):
+		self.starts = #some stuff here
+
 	# returns top 10 matches
 	def get_matches(self):
 		# some code here about getting the melody
 		return
+
+# object hook for getting a song object from a json string
+def as_song(dic):
+	file_path = dic['file_path']
+	melody = dic['melody']
+	starts = dic['starts']
+	return Song(file_path,melody,starts)
+
+# returns a song given a json string
+def to_song(str):
+	return json.loads(str,object_hook=as_song)
