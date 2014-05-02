03564dd86f495725f8d8568b8b070f0804f11dc0
Lijia Xie <lijiaxie@users.noreply.github.com>
2014-05-02 07:48:45 -0400

    process updated

--- process.py
+++ process.py
@@ -4,23 +4,29 @@ from sys import *
 from subprocess import *
 from os import *
 from string import *
+from models import titles_from_path
 
 # input_path is a folder if Hum = False
 # input_path is a filename if Hum = True
 # input_path should be unix filepath with "/" not "\"; if error, place hum audio file in the same directory as this python file
 def process(input_path, Hum=True):
+	# output filepath
+	output_path = titles_from_path(input_path) + "vamp_mtg-melodia_melodia_melody.csv"
+
 	db_path = "hum_database"
 	if Hum == False:
 		db_path = "song_database"
 		input_path = input_path + " -r"
 
 	shell_arg = ".\sonic_annotator\sonic-annotator -d vamp:mtg-melodia:melodia:melody " + input_path + " -w csv --csv-basedir " + db_path
-	
+
 	# code obtained from subprocess documentation 
 	# https://docs.python.org/2/library/subprocess.html#module-subprocess
 	try:
 	    retcode = call(shell_arg, shell=True)
 	    if retcode < 0:
 	        print >>sys.stderr, "Child was terminated by signal", -retcode
+
+	    return output_path
 	except OSError as e:
 	    print >>sys.stderr, "Execution failed:", e
